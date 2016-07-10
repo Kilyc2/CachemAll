87ecf43985249a224143a5d94a307ebf4f2c1639
Kilian Cerdan Ortiz <kilyc2@gmail.com>
2016-07-10 14:47:44 +0200

    Make ui

--- app/src/main/java/com/kiliancerdan/cachemall/ui/MainActivity.java
+++ app/src/main/java/com/kiliancerdan/cachemall/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.kiliancerdan.cachemall;
+package com.kiliancerdan.cachemall.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.kiliancerdan.cachemall.R;
 
 public class MainActivity extends AppCompatActivity {
 
